a76ab2a35ca1cdddf29406a02758eb6609777eb5
sohampatil <sohampatil.2303@gmail.com>
2023-11-26 07:22:30 -0700

    Added some pie charts

--- npid-frontend/src/State.js
+++ npid-frontend/src/State.js
@@ -1,117 +1,118 @@
 import React, { useState, useEffect } from 'react';
 import { geoCentroid } from "d3-geo";
-import { ComposableMap, Geographies, Geography,Annotation,Marker } from 'react-simple-maps';
+import { ComposableMap, Geographies, Geography, Annotation, Marker } from 'react-simple-maps';
+import ReactECharts from 'echarts-for-react';
 
 const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
 
 const stateMappings = {
-'Alabama': 'AL',
-'Alaska': 'AK',
-'Arizona': 'AZ',
-'Arkansas': 'AR',
-'California': 'CA',
-'Colorado': 'CO',
-'Connecticut': 'CT',
-'Delaware': 'DE',
-'District of Columbia': 'DC',
-'Florida': 'FL',
-'Georgia': 'GA',
-'Hawaii': 'HI',
-'Idaho': 'ID',
-'Illinois': 'IL',
-'Indiana': 'IN',
-'Iowa': 'IA',
-'Kansas': 'KS',
-'Kentucky': 'KY',
-'Louisiana': 'LA',
-'Maine': 'ME',
-'Maryland': 'MD',
-'Massachusetts': 'MA',
-'Michigan': 'MI',
-'Minnesota': 'MN',
-'Mississippi': 'MS',
-'Missouri': 'MO',
-'Montana': 'MT',
-'Nebraska': 'NE',
-'Nevada': 'NV',
-'New Hampshire': 'NH',
-'New Jersey': 'NJ',
-'New Mexico': 'NM',
-'New York': 'NY',
-'North Carolina': 'NC',
-'North Dakota': 'ND',
-'Ohio': 'OH',
-'Oklahoma': 'OK',
-'Oregon': 'OR',
-'Pennsylvania': 'PA',
-'Rhode Island': 'RI',
-'South Carolina': 'SC',
-'South Dakota': 'SD',
-'Tennessee': 'TN',
-'Texas': 'TX',
-'Utah': 'UT',
-'Vermont': 'VT',
-'Virginia': 'VA',
-'Washington': 'WA',
-'West Virginia': 'WV',
-'Wisconsin': 'WI',
-'Wyoming': 'WY',
-'Puerto Rico': 'PR',
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'District of Columbia': 'DC',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+    'Puerto Rico': 'PR',
 };
 
 // Replace this with your actual data
 const murderData = {
-    'Alabama': 4822023 ,
-    'Alaska': 731449 ,
-    'Arizona': 6553255 ,
-    'Arkansas': 2949131 ,
-    'California': 38041430 ,
-    'Colorado': 5187582 ,
-    'Connecticut': 3590347 ,
-    'Delaware': 917092 ,
-    'District of Columbia': 632323 ,
-    'Florida': 19317568 ,
-    'Georgia': 9919945 ,
-    'Hawaii': 1392313 ,
-    'Idaho': 1595728 ,
-    'Illinois': 12875255 ,
-    'Indiana': 6537334 ,
-    'Iowa': 3074186 ,
-    'Kansas': 2885905 ,
-    'Kentucky': 4380415 ,
-    'Louisiana': 4601893 ,
-    'Maine': 1329192 ,
-    'Maryland': 5884563 ,
-    'Massachusetts': 6646144 ,
-    'Michigan': 9883360 ,
-    'Minnesota': 5379139 ,
-    'Mississippi': 2984926 ,
-    'Missouri': 6021988 ,
-    'Montana': 1005141 ,
-    'Nebraska': 1855525 ,
-    'Nevada': 2758931 ,
-    'New Hampshire': 1320718 ,
-    'New Jersey': 8864590 ,
-    'New Mexico': 2085538 ,
-    'New York': 19570261 ,
-    'North Carolina': 9752073 ,
-    'North Dakota': 699628 ,
-    'Ohio': 11544225 ,
-    'Oklahoma': 3814820 ,
-    'Oregon': 3899353 ,
-    'Pennsylvania': 12763536 ,
-    'Rhode Island': 1050292 ,
-    'South Carolina': 4723723 ,
-    'South Dakota': 833354 ,
-    'Tennessee': 6456243 ,
-    'Texas': 26059203 ,
-    'Utah': 2855287 ,
-    'Vermont': 626011 ,
-    'Virginia': 8185867 ,
-    'Washington': 6897012 ,
-    'West Virginia': 1855413 ,
-    'Wisconsin': 5726398 ,
-    'Wyoming': 576412 ,
+    'Alabama': 4822023,
+    'Alaska': 731449,
+    'Arizona': 6553255,
+    'Arkansas': 2949131,
+    'California': 38041430,
+    'Colorado': 5187582,
+    'Connecticut': 3590347,
+    'Delaware': 917092,
+    'District of Columbia': 632323,
+    'Florida': 19317568,
+    'Georgia': 9919945,
+    'Hawaii': 1392313,
+    'Idaho': 1595728,
+    'Illinois': 12875255,
+    'Indiana': 6537334,
+    'Iowa': 3074186,
+    'Kansas': 2885905,
+    'Kentucky': 4380415,
+    'Louisiana': 4601893,
+    'Maine': 1329192,
+    'Maryland': 5884563,
+    'Massachusetts': 6646144,
+    'Michigan': 9883360,
+    'Minnesota': 5379139,
+    'Mississippi': 2984926,
+    'Missouri': 6021988,
+    'Montana': 1005141,
+    'Nebraska': 1855525,
+    'Nevada': 2758931,
+    'New Hampshire': 1320718,
+    'New Jersey': 8864590,
+    'New Mexico': 2085538,
+    'New York': 19570261,
+    'North Carolina': 9752073,
+    'North Dakota': 699628,
+    'Ohio': 11544225,
+    'Oklahoma': 3814820,
+    'Oregon': 3899353,
+    'Pennsylvania': 12763536,
+    'Rhode Island': 1050292,
+    'South Carolina': 4723723,
+    'South Dakota': 833354,
+    'Tennessee': 6456243,
+    'Texas': 26059203,
+    'Utah': 2855287,
+    'Vermont': 626011,
+    'Virginia': 8185867,
+    'Washington': 6897012,
+    'West Virginia': 1855413,
+    'Wisconsin': 5726398,
+    'Wyoming': 576412,
     'Puerto Rico': 3667084,
 };
 const offsets = {
@@ -124,91 +125,219 @@ const offsets = {
     DE: [33, 0],
     MD: [47, 10],
     DC: [49, 21]
-  };
-  
+};
+
+const crimeData = {
+    'Male': 5000,
+    'Female': 3000
+};
+
+const raceCrimeData = {
+    'White': 5000,
+    'Black': 3000,
+    'Native American': 2000,
+    'Asian': 1000,
+    'Hispanic': 4000
+};
+
+const mannerOfDeathData = {
+    'Shot': 6000,
+    'Shot and Tasered': 4000
+};
 
 function State() {
-  const maxMurder = Math.max(...Object.values(murderData));
-  const [selectedYears, setSelectedYears] = useState(['2015', '2016', '2017']);
+    const maxMurder = Math.max(...Object.values(murderData));
+    const [selectedYears, setSelectedYears] = useState(['2015', '2016', '2017']);
 
-  const handleCheckboxChange = (event) => {
-    const year = event.target.value;
-    setSelectedYears((prevSelectedYears) => {
-        const updatedYears = event.target.checked
-          ? [...prevSelectedYears, year]
-          : prevSelectedYears.filter((selectedYear) => selectedYear !== year);
-        return updatedYears;
-      });
-  };
-  useEffect(() => {
-    console.log(selectedYears);
-  }, [selectedYears]);
-  return (
-    <div>
-      <label>
-        Select Years:    
-      </label>
-      <label>
-        <input type="checkbox" value="2015" onChange={handleCheckboxChange} checked={selectedYears.includes('2015')}/>
-        2015
-      </label>
-      <label>
-        <input type="checkbox" value="2016" onChange={handleCheckboxChange} checked={selectedYears.includes('2016')}/>
-        2016
-      </label>
-      <label>
-        <input type="checkbox" value="2017" onChange={handleCheckboxChange} checked={selectedYears.includes('2017')}/>
-        2017
-      </label>
-      <ComposableMap projection="geoAlbersUsa">
-        <Geographies geography={geoUrl}>
-        {({ geographies }) => (
-          <>
-            {geographies.map(geo => {
-              const cur = murderData[geo.properties.name];
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill={cur ? `rgba(44, 62, 80, ${0.1 + 0.9 * cur / maxMurder})` : "#D3D3D3"}
-                  onClick={() => console.log(stateMappings[geo.properties.name])}
-                />
-              );
-            })}
-            {geographies.map(geo => {
-            const centroid = geoCentroid(geo);
-            const cur = Object.keys(stateMappings).find(s => stateMappings[s] === stateMappings[geo.properties.name]);
-            return (
-            <g key={geo.rsmKey + "-name"}>
-                {cur &&
-                centroid[0] > -160 &&
-                centroid[0] < -67 &&
-                (Object.keys(offsets).indexOf(stateMappings[cur]) === -1 ? (
-                    <Marker coordinates={centroid}>
-                    <text y="2" fontSize={10} textAnchor="middle">
-                        {stateMappings[cur]}
-                    </text>
-                    </Marker>
-                ) : (
-                    <Annotation
-                    subject={centroid}
-                    dx={offsets[stateMappings[cur]][0]}
-                    dy={offsets[stateMappings[cur]][1]}
-                    >
-                    <text x={4} fontSize={10} alignmentBaseline="middle">
-                    {stateMappings[cur]}
-                    </text>
-                    </Annotation>
-                ))}
-            </g>
-            );
-            })}
-          </>
-        )}
-      </Geographies>
-      </ComposableMap>
-    </div>
-  );
+    const handleCheckboxChange = (event) => {
+        const year = event.target.value;
+        setSelectedYears((prevSelectedYears) => {
+            const updatedYears = event.target.checked
+                ? [...prevSelectedYears, year]
+                : prevSelectedYears.filter((selectedYear) => selectedYear !== year);
+            return updatedYears;
+        });
+    };
+    useEffect(() => {
+        console.log(selectedYears);
+    }, [selectedYears]);
+    const option = {
+        title: {
+            text: 'Gender Crime Data',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'item'
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left'
+        },
+        series: [
+            {
+                name: 'Crimes By',
+                type: 'pie',
+                radius: '50%',
+                data: [
+                    { value: crimeData['Male'], name: 'Male' },
+                    { value: crimeData['Female'], name: 'Female' }
+                ],
+                color: ['green', 'red'],
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+
+    const raceOption = {
+        title: {
+            text: 'Race Crime Data',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'item'
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left'
+        },
+        series: [
+            {
+                name: 'Crimes By',
+                type: 'pie',
+                radius: '50%',
+                data: Object.entries(raceCrimeData).map(([name, value]) => ({ name, value })),
+                color: ['green', 'red', 'blue', 'yellow', 'purple'],
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+
+    const mannerOfDeathOption = {
+        title: {
+            text: 'Manner of Death Data',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'item'
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left'
+        },
+        series: [
+            {
+                name: 'Deaths By',
+                type: 'pie',
+                radius: '50%',
+                data: Object.entries(mannerOfDeathData).map(([name, value]) => ({ name, value })),
+                color: ['blue', 'orange'],
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+
+    return (
+        <div style={{height: '40%'}}>
+            <div>
+                <label>
+                        Select Years:
+                    </label>
+                    <label>
+                        <input type="checkbox" value="2015" onChange={handleCheckboxChange} checked={selectedYears.includes('2015')} />
+                        2015
+                    </label>
+                    <label>
+                        <input type="checkbox" value="2016" onChange={handleCheckboxChange} checked={selectedYears.includes('2016')} />
+                        2016
+                    </label>
+                    <label>
+                        <input type="checkbox" value="2017" onChange={handleCheckboxChange} checked={selectedYears.includes('2017')} />
+                        2017
+                </label>
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                
+                <div style={{ width: '45%' }}>
+                    <ComposableMap projection="geoAlbersUsa">
+                        <Geographies geography={geoUrl}>
+                            {({ geographies }) => (
+                                <>
+                                    {geographies.map(geo => {
+                                        const cur = murderData[geo.properties.name];
+                                        return (
+                                            <Geography
+                                                key={geo.rsmKey}
+                                                geography={geo}
+                                                fill={cur ? `rgba(44, 62, 80, ${0.1 + 0.9 * cur / maxMurder})` : "#D3D3D3"}
+                                                onClick={() => console.log(stateMappings[geo.properties.name])}
+                                            />
+                                        );
+                                    })}
+                                    {geographies.map(geo => {
+                                        const centroid = geoCentroid(geo);
+                                        const cur = Object.keys(stateMappings).find(s => stateMappings[s] === stateMappings[geo.properties.name]);
+                                        return (
+                                            <g key={geo.rsmKey + "-name"}>
+                                                {cur &&
+                                                    centroid[0] > -160 &&
+                                                    centroid[0] < -67 &&
+                                                    (Object.keys(offsets).indexOf(stateMappings[cur]) === -1 ? (
+                                                        <Marker coordinates={centroid}>
+                                                            <text y="2" fontSize={10} textAnchor="middle">
+                                                                {stateMappings[cur]}
+                                                            </text>
+                                                        </Marker>
+                                                    ) : (
+                                                        <Annotation
+                                                            subject={centroid}
+                                                            dx={offsets[stateMappings[cur]][0]}
+                                                            dy={offsets[stateMappings[cur]][1]}
+                                                        >
+                                                            <text x={4} fontSize={10} alignmentBaseline="middle">
+                                                                {stateMappings[cur]}
+                                                            </text>
+                                                        </Annotation>
+                                                    ))}
+                                            </g>
+                                        );
+                                    })}
+                                </>
+                            )}
+                        </Geographies>
+                    </ComposableMap>
+                </div>
+                <div style={{ width: '25%', height: '40%' }}>
+                    <div style={{ height: '20%' }}>
+                        <ReactECharts option={option} />
+                    </div>
+                    <div style={{ height: '20%' }}>
+                        <ReactECharts option={raceOption} />
+                    </div>
+                    <div style={{ height: '20%' }}>
+                        <ReactECharts option={mannerOfDeathOption} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default State;
