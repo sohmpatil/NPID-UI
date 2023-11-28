64f5a9c35459d94f1ee5ec48e7fdb56915519f04
Sanket Kapse <sanket8397@gmail.com>
2023-11-28 00:46:43 -0700

    Detailed Tab connected to API

--- npid-frontend/src/Detailed.js
+++ npid-frontend/src/Detailed.js
@@ -1,311 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Detailed.css';
 
 function Detailed() {
-  const data = [
-    {
-      name: 'John Doe',
-      race: 'White',
-      age: 30,
-      deathYear: 2020,
-      gender: 'Male',
-      mentalillness: 'No',
-      mannerofdeath: 'Shot',
-      weapon: 'Gun',
-    },
-    {
-        name: 'Roger Fedrer',
-        race: 'White',
-        age: 30,
-        deathYear: 2020,
-        gender: 'Male',
-        mentalillness: 'No',
-        mannerofdeath: 'Shot',
-        weapon: 'Gun',
-      },
-      {
-        name: 'John Doe',
-        race: 'White',
-        age: 30,
-        deathYear: 2020,
-        gender: 'Male',
-        mentalillness: 'No',
-        mannerofdeath: 'Shot',
-        weapon: 'Gun',
-      },
-      {
-          name: 'Roger Fedrer',
-          race: 'White',
-          age: 30,
-          deathYear: 2020,
-          gender: 'Male',
-          mentalillness: 'No',
-          mannerofdeath: 'Shot',
-          weapon: 'Gun',
-        },
-        {
-            name: 'John Doe',
-            race: 'White',
-            age: 30,
-            deathYear: 2020,
-            gender: 'Male',
-            mentalillness: 'No',
-            mannerofdeath: 'Shot',
-            weapon: 'Gun',
-          },
-          {
-              name: 'Roger Fedrer',
-              race: 'White',
-              age: 30,
-              deathYear: 2020,
-              gender: 'Male',
-              mentalillness: 'No',
-              mannerofdeath: 'Shot',
-              weapon: 'Gun',
-            },
-            {
-              name: 'John Doe',
-              race: 'White',
-              age: 30,
-              deathYear: 2020,
-              gender: 'Male',
-              mentalillness: 'No',
-              mannerofdeath: 'Shot',
-              weapon: 'Gun',
-            },
-            {
-                name: 'Roger Fedrer',
-                race: 'White',
-                age: 30,
-                deathYear: 2020,
-                gender: 'Male',
-                mentalillness: 'No',
-                mannerofdeath: 'Shot',
-                weapon: 'Gun',
-              },
-              {
-                name: 'John Doe',
-                race: 'White',
-                age: 30,
-                deathYear: 2020,
-                gender: 'Male',
-                mentalillness: 'No',
-                mannerofdeath: 'Shot',
-                weapon: 'Gun',
-              },
-              {
-                  name: 'Roger Fedrer',
-                  race: 'White',
-                  age: 30,
-                  deathYear: 2020,
-                  gender: 'Male',
-                  mentalillness: 'No',
-                  mannerofdeath: 'Shot',
-                  weapon: 'Gun',
-                },
-                {
-                  name: 'John Doe',
-                  race: 'White',
-                  age: 30,
-                  deathYear: 2020,
-                  gender: 'Male',
-                  mentalillness: 'No',
-                  mannerofdeath: 'Shot',
-                  weapon: 'Gun',
-                },
-                {
-                    name: 'Roger Fedrer',
-                    race: 'White',
-                    age: 30,
-                    deathYear: 2020,
-                    gender: 'Male',
-                    mentalillness: 'No',
-                    mannerofdeath: 'Shot',
-                    weapon: 'Gun',
-                  },
-                  {
-                      name: 'John Doe',
-                      race: 'White',
-                      age: 30,
-                      deathYear: 2020,
-                      gender: 'Male',
-                      mentalillness: 'No',
-                      mannerofdeath: 'Shot',
-                      weapon: 'Gun',
-                    },
-                    {
-                        name: 'Roger Fedrer',
-                        race: 'White',
-                        age: 30,
-                        deathYear: 2020,
-                        gender: 'Male',
-                        mentalillness: 'No',
-                        mannerofdeath: 'Shot',
-                        weapon: 'Gun',
-                      },
-                      {
-                        name: 'John Doe',
-                        race: 'White',
-                        age: 30,
-                        deathYear: 2020,
-                        gender: 'Male',
-                        mentalillness: 'No',
-                        mannerofdeath: 'Shot',
-                        weapon: 'Gun',
-                      },
-                      {
-                          name: 'Roger Fedrer',
-                          race: 'White',
-                          age: 30,
-                          deathYear: 2020,
-                          gender: 'Male',
-                          mentalillness: 'No',
-                          mannerofdeath: 'Shot',
-                          weapon: 'Gun',
-                        },
-                        {
-                            name: 'John Doe',
-                            race: 'White',
-                            age: 30,
-                            deathYear: 2020,
-                            gender: 'Male',
-                            mentalillness: 'No',
-                            mannerofdeath: 'Shot',
-                            weapon: 'Gun',
-                          },
-                          {
-                              name: 'Roger Fedrer',
-                              race: 'White',
-                              age: 30,
-                              deathYear: 2020,
-                              gender: 'Male',
-                              mentalillness: 'No',
-                              mannerofdeath: 'Shot',
-                              weapon: 'Gun',
-                            },
-                            {
-                              name: 'John Doe',
-                              race: 'White',
-                              age: 30,
-                              deathYear: 2020,
-                              gender: 'Male',
-                              mentalillness: 'No',
-                              mannerofdeath: 'Shot',
-                              weapon: 'Gun',
-                            },
-                            {
-                                name: 'Roger Fedrer',
-                                race: 'White',
-                                age: 30,
-                                deathYear: 2020,
-                                gender: 'Male',
-                                mentalillness: 'No',
-                                mannerofdeath: 'Shot',
-                                weapon: 'Gun',
-                              },
-                              {
-                                  name: 'John Doe',
-                                  race: 'White',
-                                  age: 30,
-                                  deathYear: 2020,
-                                  gender: 'Male',
-                                  mentalillness: 'No',
-                                  mannerofdeath: 'Shot',
-                                  weapon: 'Gun',
-                                },
-                                {
-                                    name: 'Roger Fedrer',
-                                    race: 'White',
-                                    age: 30,
-                                    deathYear: 2020,
-                                    gender: 'Male',
-                                    mentalillness: 'No',
-                                    mannerofdeath: 'Shot',
-                                    weapon: 'Gun',
-                                  },
-                                  {
-                                    name: 'John Doe',
-                                    race: 'White',
-                                    age: 30,
-                                    deathYear: 2020,
-                                    gender: 'Male',
-                                    mentalillness: 'No',
-                                    mannerofdeath: 'Shot',
-                                    weapon: 'Gun',
-                                  },
-                                  {
-                                      name: 'Roger Fedrer',
-                                      race: 'White',
-                                      age: 30,
-                                      deathYear: 2020,
-                                      gender: 'Male',
-                                      mentalillness: 'No',
-                                      mannerofdeath: 'Shot',
-                                      weapon: 'Gun',
-                                    },
-    // Add more items here...
-  ];
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Fetch table chart data
+    axios.get('http://127.0.0.1:5000/api/details')
+      .then(response => setData(response.data))
+      .catch(error => console.error('Error fetching table data:', error));
+  }, []);
 
   return (
     <div className="Detailed Detailed-Table">
+      {data ?
         <table>
-            <tr>
-                <th>Name</th>
-                <th>Race</th>
-                <th>Age</th>
-                <th>Death Year</th>
-                <th>Gender</th>
-                <th>Mental Illness</th>
-                <th>Manner of Death</th>
-                <th>Weapon</th>
-            </tr>
-            {data.map((val, key) => {
-                return (
-                    <tr key={key}>
-                        <td>{val.name}</td>
-                        <td>{val.race}</td>
-                        <td>{val.age}</td>
-                        <td>{val.deathYear}</td>
-                        <td>{val.gender}</td>
-                        <td>{val.mentalillness}</td>
-                        <td>{val.mannerofdeath}</td>
-                        <td>{val.weapon}</td>
-                    </tr>
-                )
-            })}
+          <tr>
+            <th>Name</th>
+            <th>Race</th>
+            <th>Age</th>
+            <th>Death Year</th>
+            <th>Gender</th>
+            <th>Mental Illness</th>
+            <th>Manner of Death</th>
+            <th>Weapon</th>
+          </tr>
+          {data.map((val, key) => {
+            return (
+              <tr key={key}>
+                <td>{val.name}</td>
+                <td>{val.race}</td>
+                <td>{val.age}</td>
+                <td>{val.deathYear}</td>
+                <td>{val.gender}</td>
+                <td>{val.mentalillness}</td>
+                <td>{val.mannerofdeath}</td>
+                <td>{val.weapon}</td>
+              </tr>
+            )
+          })}
         </table>
+        : 'Loading Table Data'
+      }
     </div>
-);
-//   return (
-//     <table>
-//       <thead>
-//         <tr>
-//           <th>Name</th>
-//           <th>Race</th>
-//           <th>Age</th>
-//           <th>Death Year</th>
-//           <th>Gender</th>
-//           <th>Mental Illness</th>
-//           <th>Manner of Death</th>
-//           <th>Weapon</th>
-//         </tr>
-//       </thead>
-//       <tbody>
-//         {data.map((item, index) => (
-//           <tr key={index}>
-//             <td>{item.name}</td>
-//             <td>{item.race}</td>
-//             <td>{item.age}</td>
-//             <td>{item.deathYear}</td>
-//             <td>{item.gender}</td>
-//             <td>{item.mentalillness}</td>
-//             <td>{item.mannerofdeath}</td>
-//             <td>{item.weapon}</td>
-//           </tr>
-//         ))}
-//       </tbody>
-//     </table>
-//   );
+  );
 }
 
 export default Detailed;
