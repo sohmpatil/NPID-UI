312b57b2a35b7c537d61dd61164f01f79289a78e
Nirmit Agrawal <nagraw18@asu.edu>
2023-11-30 22:57:14 -0700

    home-page v1.1

--- npid-frontend/src/Home.js
+++ npid-frontend/src/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Home.css';
+
 function Home() {
   return (
     <div className='home-container'>
       <h1>National Police Incident Database</h1>
+      <p>Welcome to the National Police Incident Database, a crucial platform addressing the need for transparency and accountability in law enforcement. This app aims to collect, analyze, and visualize data related to police-involved incidents across the United States, providing users with insights into policing practices. By promoting data-driven decision-making, NPID contributes to fostering a more informed and just society. The app offers essential details such as location, involved parties, and descriptions, placing a strong focus on data accuracy and validation to ensure the reliability of the information. Additionally, it features an interactive mapping system, powerful data analysis tools, and transparency ratings for participating law enforcement agencies.</p>
     </div>
   );
 }
